61a3c97e4d4158d7d3c7e729639d80d39d8dcb4f
Gabriel Riquetto <gabrielriquetto17@hotmail.com>
2022-03-06 16:56:27 -0300

    update

--- web.py
+++ web.py
@@ -129,7 +129,7 @@ def retorna_mensagem(lista):
     for index in lista:
         for loja, valor in index.items():
             try:
-                mensagem += f"Loja: {loja}\n"
+                mensagem += f"\n\nLoja: {loja}\n"
                 for chave, value in valor.items():
                     try:
                         mensagem += f"Produto: {value['nome']}\n\n"
