4edd1a22b4717b0be3f8c978bd2a2d95c8982f68
Gabriel Riquetto <gabrielriquetto17@hotmail.com>
2022-03-07 20:33:10 -0300

    Class Amazon

---
+++ Classes/ClassAmazon.py
@@ -0,0 +1,87 @@
+from Libraries.imports import *
+
+class Amazon:
+    def __init__(self, lista, product):
+        self.product = product.replace(" ", "+")
+        self.lista = lista
+
+    def isWorking(self):
+        erro = 0
+        url = f'https://www.amazon.com.br/s?k={self.product}'
+        while True:
+            site = requests.get(url)
+            soup = BeautifulSoup(site.content, 'html.parser')
+            informations = soup.find_all("div", class_="a-spacing-small")
+            if informations != []:
+                return informations
+            elif erro == 250 and site.status_code != 200:
+                return site.status_code
+            elif erro == 250:
+                return False
+            erro += 1
+    
+    def __get_link(self, i, informations, dict_temp):
+        formated = str(informations[i])
+        comeco_link = formated.find('href="')
+        final_link = formated[comeco_link:].find('"><')
+        comeco = formated[comeco_link:]
+        completed = comeco[:final_link].replace('href="', "")
+        dict_temp['link'] = f"amazon.com.br{completed}"   
+    
+    def __get_name(self, estrelas, texto, dict_temp):
+        dict_temp['nome'] = texto[:estrelas-10].rstrip().lstrip()
+
+    def __get_price(self, cifrao, texto, dict_temp):
+        virgula = texto[cifrao:].find(",")
+        comeco_preco = texto[cifrao:]
+        dict_temp['regular price'] = comeco_preco[:virgula+3]
+
+    def __get_portion(self, texto, dict_temp):
+        try:
+            parcela_comeco = texto.find("em até")
+            if parcela_comeco != -1:
+                parcela_final = texto[parcela_comeco:].find("juros")
+                primeiro = texto[parcela_comeco:]
+                dict_temp['parcela'] = primeiro[:parcela_final+5].rstrip().lstrip()
+        except:
+            pass
+    
+    def __get_evaluation(self, texto, estrelas, cifrao, dict_temp):
+        avaliacao = texto[estrelas-10: cifrao].rstrip().lstrip()
+        if "Economize" in avaliacao:
+            economize = avaliacao.find("Economize")
+            dict_temp['avaliacao'] = f"{avaliacao[:economize].rstrip().lstrip()} avaliações"
+        else:
+            dict_temp['avaliacao'] = f"{avaliacao} avaliações"
+
+    def search_product_amazon(self):
+        lista = self.lista
+        i = 0
+        count = 0
+        dict_append = {}
+        informations = self.isWorking()
+        if informations != False:
+            while count < 3:
+                if "R$" in informations[i].get_text():
+                    dict_temp = {}
+                    texto = str(informations[i].get_text())
+                    estrelas = texto.find("estrelas")
+                    cifrao = texto.find("R$")
+                    
+                    self.__get_link(i, informations, dict_temp)
+                    self.__get_name(estrelas, texto, dict_temp)
+                    self.__get_price(cifrao, texto, dict_temp)
+                    self.__get_portion(texto, dict_temp)
+                    self.__get_evaluation(texto, estrelas, cifrao, dict_temp)
+                    
+                    dict_append[count] = dict_temp
+                    count+=1
+                i+=1
+            lista.append({"Amazon":dict_append})
+            return lista
+        elif informations != 200:
+            lista.append({"Amazon":"Amazon está com algum problema..."})
+            return lista
+        else:
+            lista.append({"Amazon":"Ocorreu algum problema comigo!"})
+            return lista
--- Web/web.py
+++ Web/web.py
@@ -1,68 +1,7 @@
 from Libraries.imports import *
 from Classes.ClassPichau import Pichau
 from Classes.ClassKabum import Kabum
-
-def search_product_amazon(lista, product):
-    lista = lista
-    product = product.replace(" ", "+")
-    i = 0
-    erro = 0
-    count = 0
-    dict_append = {}
-    url = f'https://www.amazon.com.br/s?k={product}'
-    while True:
-        site = requests.get(url)
-        soup = BeautifulSoup(site.content, 'html.parser')
-        informations = soup.find_all("div", class_="a-spacing-small")
-        if informations != []:
-            break
-        elif erro == 250:
-            break
-        erro += 1
-    if informations !=[]:
-        while count < 3:
-            if "R$" in informations[i].get_text():
-                dict_temp = {}
-                texto = str(informations[i].get_text())
-
-                formated = str(informations[i])
-                comeco_link = formated.find('href="')
-                final_link = formated[comeco_link:].find('"><')
-                comeco = formated[comeco_link:]
-                completed = comeco[:final_link].replace('href="', "")
-                dict_temp['link'] = f"amazon.com.br{completed}"    
-                estrelas = texto.find("estrelas")
-
-                cifrao = texto.find("R$")
-                virgula = texto[cifrao:].find(",")
-                comeco_preco = texto[cifrao:]
-                dict_temp['regular price'] = comeco_preco[:virgula+3]
-                try:
-                    parcela_comeco = texto.find("em até")
-                    if parcela_comeco != -1:
-                        parcela_final = texto[parcela_comeco:].find("juros")
-                        primeiro = texto[parcela_comeco:]
-                        dict_temp['parcela'] = primeiro[:parcela_final+5].rstrip().lstrip()
-                except:
-                    pass
-                dict_temp['nome'] = texto[:estrelas-10].rstrip().lstrip()
-                avaliacao = texto[estrelas-10: cifrao].rstrip().lstrip()
-                if "Economize" in avaliacao:
-                    economize = avaliacao.find("Economize")
-                    dict_temp['avaliacao'] = f"{avaliacao[:economize].rstrip().lstrip()} avaliações"
-                else:
-                    dict_temp['avaliacao'] = f"{avaliacao} avaliações"
-                dict_append[count] = dict_temp
-                count+=1
-            i+=1
-        lista.append({"Amazon":dict_append})
-        return lista
-    elif site.status_code != 200:
-        lista.append({"Amazon":"Amazon está com algum problema..."})
-        return lista
-    else:
-        lista.append({"Amazon":"Ocorreu algum problema comigo!"})
-        return lista
+from Classes.ClassAmazon import Amazon
 
 def return_message(lista):
     mensagem = "Essas foram as informações que eu obtive:\n"
@@ -113,7 +52,7 @@ def return_message(lista):
 
 def main(product):
     listaK = Kabum(product).search_product_kabum()
-    listaKA = search_product_amazon(listaK, product)
+    listaKA = Amazon(listaK, product).search_product_amazon()
     listaKAP = Pichau(listaKA, product).search_product_pichau()
     message = return_message(listaKAP)
     return message
